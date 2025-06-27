6e2877d301cbc6b610be7e911d5a012af2ba86cb
Olek <aleksander.garbacz@gmail.com>
2025-06-27 13:23:40 +0200

    this is terrible

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
+echo "📦 Starting entrypoint..."
+
 if [ ! -d /var/lib/mysql/mysql ]; then
     echo "🛠 Initializing MariaDB system tables..."
-    mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql > /dev/null
+    mariadb-install-db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
 
     echo "▶ Bootstrapping with init.sql..."
     mysqld --user=mysql --bootstrap < /docker-entrypoint-initdb.d/init.sql
+    echo "✅ Bootstrapping done."
 else
     echo "✅ Existing database detected, skipping init."
 fi
 
+echo "🚀 Starting MariaDB..."
 exec mysqld --user=mysql --console
